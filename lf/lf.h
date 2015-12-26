1802420e7045e60d607a01e17e8d03a7848df0dd
shogo4405 <shogo4405@gmail.com>
2015-12-26 20:26:15 +0900

    ディレクトリの構造の修正

--- lf/lf.h
+++ lf/lf.h
@@ -1,11 +1,3 @@
 #import <UIKit/UIKit.h>
-
-//! Project version number for lf.
 FOUNDATION_EXPORT double lfVersionNumber;
-
-//! Project version string for lf.
 FOUNDATION_EXPORT const unsigned char lfVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <lf/PublicHeader.h>
-
-
