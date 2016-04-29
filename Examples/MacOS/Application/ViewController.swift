636915e56d0aedeefd07aa0f1cbec741f93fd915
shogo4405 <shogo4405@gmail.com>
2016-04-29 16:57:36 +0900

    Examplesの整理

---
+++ Examples/MacOS/Application/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Application
+//
+//  Created by Shogo Endo on 2016/04/29.
+//  Copyright © 2016年 Shogo Endo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
