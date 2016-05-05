6f2578ae257d6e9b3eb361bff99d01285e2760f3
shogo4405 <shogo4405@gmail.com>
2016-05-05 20:47:39 +0900

    管理ディレクトリの修正

--- Examples/MacOS/LiveViewController.swift
+++ Examples/MacOS/LiveViewController.swift
@@ -4,7 +4,8 @@ import AVFoundation
 
 final class LiveViewController: NSViewController {
 
-    var popUpButton:NSPopUpButton!
+    var cameraPopUpButton:NSPopUpButton!
+    var audioPopUpButton:NSPopUpButton!
     var publishButton:NSButton!
 
     var url:String = "rtmp://test:test@localhost/live"
@@ -28,21 +29,33 @@ final class LiveViewController: NSViewController {
         publishButton.action = #selector(LiveViewController.publishOrStop(_:))
         publishButton.target = self
 
-        popUpButton = NSPopUpButton()
-        popUpButton.action = #selector(LiveViewController.selectCamera(_:))
-        popUpButton.target = self
+        cameraPopUpButton = NSPopUpButton()
+        cameraPopUpButton.action = #selector(LiveViewController.selectCamera(_:))
+        cameraPopUpButton.target = self
         let cameras:[AnyObject]! = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
         for camera in cameras {
             if let camera:AVCaptureDevice = camera as? AVCaptureDevice {
-                popUpButton.addItemWithTitle(camera.localizedName)
+                cameraPopUpButton.addItemWithTitle(camera.localizedName)
             }
         }
         if (!cameras.isEmpty) {
             rtmpStream.attachCamera(cameras[0] as? AVCaptureDevice)
         }
 
+        audioPopUpButton = NSPopUpButton()
+        let audios:[AnyObject]! = AVCaptureDevice.devicesWithMediaType(AVMediaTypeAudio)
+        for audio in audios {
+            if let audio:AVCaptureDevice = audio as? AVCaptureDevice {
+                audioPopUpButton.addItemWithTitle(audio.localizedName)
+            }
+        }
+        if (!audios.isEmpty) {
+            rtmpStream.attachAudio(audios[0] as? AVCaptureDevice)
+        }
+
         view.addSubview(rtmpStream.view)
-        view.addSubview(popUpButton)
+        view.addSubview(cameraPopUpButton)
+        view.addSubview(audioPopUpButton)
         view.addSubview(publishButton)
     }
 
@@ -50,12 +63,13 @@ final class LiveViewController: NSViewController {
         super.viewWillLayout()
         rtmpStream.view.frame = view.frame
         publishButton.frame = NSMakeRect(view.frame.width - 120, view.frame.height - 40, 100, 20)
-        popUpButton.frame = NSMakeRect(view.frame.width - 220, 20, 200, 20)
+        cameraPopUpButton.frame = NSMakeRect(view.frame.width - 220, 50, 200, 20)
+        audioPopUpButton.frame = NSMakeRect(view.frame.width - 220, 20, 200, 20)
     }
 
     func selectCamera(sender:AnyObject) {
         let devices:[AnyObject]! = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
-        let title:String = popUpButton.itemTitles[popUpButton.indexOfSelectedItem]
+        let title:String = cameraPopUpButton.itemTitles[cameraPopUpButton.indexOfSelectedItem]
         for device in devices {
             guard let device:AVCaptureDevice = device as? AVCaptureDevice
                 where device.localizedName == title else {
