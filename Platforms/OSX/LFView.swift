3ce6d11aeb5a3cc181116ba8b343d18055fb68b4
shogo4405 <shogo4405@gmail.com>
2016-07-20 23:25:51 +0900

    add LFView macOS

--- Platforms/OSX/LFView.swift
+++ Platforms/OSX/LFView.swift
@@ -1,5 +1,43 @@
 import Foundation
+import AVFoundation
 
 public class LFView: NSView {
-    
+
+    public static var defaultBackgroundColor:NSColor = NSColor.blackColor()
+
+    public var videoGravity:String! = AVLayerVideoGravityResize {
+        didSet {
+            switch videoGravity {
+            case AVLayerVideoGravityResizeAspect:
+                layer?.contentsGravity = kCAGravityResizeAspect
+            case AVLayerVideoGravityResizeAspectFill:
+                layer?.contentsGravity = kCAGravityResizeAspectFill
+            case AVLayerVideoGravityResize:
+                layer?.contentsGravity = kCAGravityResize
+            default:
+                layer?.contentsGravity = kCAGravityResizeAspect
+            }
+        }
+    }
+
+    override public init(frame: NSRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+
+    public func attachStream(stream:Stream?) {
+        layer?.setValue(stream?.mixer.session, forKey: "session")
+    }
+
+    private func initialize() {
+        wantsLayer = true
+        layer = AVCaptureVideoPreviewLayer()
+        layer?.backgroundColor = LFView.defaultBackgroundColor.CGColor
+        layer?.setValue(videoGravity, forKey: "videoGravity")
+    }
 }
