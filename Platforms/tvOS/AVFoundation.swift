88367da291e4d646f5d42bf0357af7c39e827f97
shogo4405 <shogo4405@gmail.com>
2017-09-25 00:36:07 +0900

    Migration 4.0

--- Platforms/tvOS/AVFoundation.swift
+++ Platforms/tvOS/AVFoundation.swift
@@ -5,9 +5,9 @@ typealias AVCaptureOutput = Any
 typealias AVCaptureConnection = Any
 
 protocol AVCaptureVideoDataOutputSampleBufferDelegate {
-    func captureOutput(_ captureOutput:AVCaptureOutput!, didOutputSampleBuffer sampleBuffer:CMSampleBuffer!, from connection:AVCaptureConnection!)
+    func captureOutput(_ captureOutput:AVCaptureOutput, didOutput sampleBuffer:CMSampleBuffer, from connection:AVCaptureConnection)
 }
 
 protocol AVCaptureAudioDataOutputSampleBufferDelegate {
-    func captureOutput(_ captureOutput:AVCaptureOutput!, didOutputSampleBuffer sampleBuffer:CMSampleBuffer!, from connection:AVCaptureConnection!)
+    func captureOutput(_ captureOutput:AVCaptureOutput, didOutput sampleBuffer:CMSampleBuffer, from connection:AVCaptureConnection)
 }
