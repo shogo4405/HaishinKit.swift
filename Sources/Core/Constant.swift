0a43c8dfd01e3fb6b25a3dfd86ecb4fea410aedb
shogo4405 <shogo4405@gmail.com>
2017-11-06 02:13:07 +0900

    refs #331

--- Sources/Core/Constant.swift
+++ Sources/Core/Constant.swift
@@ -1,6 +1,6 @@
 import Logboard
 
-let logger:Logboard = Logboard.with(HaishinKit.identifier)
+let logger:Logboard = Logboard.with(HaishinKitIdentifier)
 
 public enum CMSampleBufferType: String {
     case video = "video"
