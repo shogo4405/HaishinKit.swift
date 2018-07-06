d40b50bcc5a763edf5e96c6776caefe8990c3fe7
shogo4405 <shogo4405@gmail.com>
2018-07-06 12:18:53 +0900

    Update TestsCase

--- Tests/RTMP/RTMPConnectionTests.swift
+++ Tests/RTMP/RTMPConnectionTests.swift
@@ -9,7 +9,7 @@ final class RTMPConnectionTests: XCTestCase {
         let url: URL = URL(fileURLWithPath: bundle.path(forResource: "SampleVideo_360x240_5mb-base", ofType: "mp4")!)
         let connection: RTMPConnection = RTMPConnection()
         let stream: RTMPStream = RTMPStream(connection: connection)
-        connection.connect("rtmp: //localhost: 1935/live")
+        connection.connect("rtmp://localhost:1935/live")
         stream.appendFile(url)
         stream.publish("live")
         sleep(10000)
