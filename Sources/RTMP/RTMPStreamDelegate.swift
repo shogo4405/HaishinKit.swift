bc9d2dbeca835fc26f64367837b1f54fe630640c
dawidvdh <dawidvdh@gmail.com>
2019-06-25 10:31:26 +0200

    add in did publish sufficient

--- Sources/RTMP/RTMPStreamDelegate.swift
+++ Sources/RTMP/RTMPStreamDelegate.swift
@@ -1,4 +1,5 @@
 public protocol RTMPStreamDelegate: class {
     func didPublishInsufficientBW(_ stream: RTMPStream, withConnection: RTMPConnection)
+    func didPublishSufficientBW(_ stream: RTMPStream, withConnection: RTMPConnection)
     func clear()
 }
