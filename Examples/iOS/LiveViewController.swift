29521f17dcb17bbb038d2c436196032d1686f78b
leo150 <sokolov.lev95@gmail.com>
2019-08-21 11:51:28 -0700

    TEMP: Add error handler to reproduce connection issue

--- Examples/iOS/LiveViewController.swift
+++ Examples/iOS/LiveViewController.swift
@@ -130,11 +130,15 @@ final class LiveViewController: UIViewController {
             UIApplication.shared.isIdleTimerDisabled = false
             rtmpConnection.close()
             rtmpConnection.removeEventListener(Event.RTMP_STATUS, selector: #selector(rtmpStatusHandler), observer: self)
+            rtmpConnection.removeEventListener(Event.IO_ERROR, selector: #selector(rtmpErrorHandler), observer: self)
             publish.setTitle("●", for: [])
         } else {
             UIApplication.shared.isIdleTimerDisabled = true
             rtmpConnection.addEventListener(Event.RTMP_STATUS, selector: #selector(rtmpStatusHandler), observer: self)
-            rtmpConnection.connect(Preference.defaultInstance.uri!)
+            rtmpConnection.addEventListener(Event.IO_ERROR, selector: #selector(rtmpErrorHandler), observer: self)
+            /*rtmpConnection.connect(Preference.defaultInstance.uri!)*/
+            // FIXME: Remove
+            rtmpConnection.connect("rtmp://fake.tv")
             publish.setTitle("■", for: [])
         }
         publish.isSelected = !publish.isSelected
@@ -162,7 +166,17 @@ final class LiveViewController: UIViewController {
             break
         }
     }
-
+    
+    @objc
+    private func rtmpErrorHandler(_ notification: Notification) {
+        let e = Event.from(notification)
+        print("rtmpErrorHandler: \(e.description)")
+    
+        DispatchQueue.main.async {
+            self.rtmpConnection.connect(Preference.defaultInstance.uri!)
+        }
+    }
+    
     func tapScreen(_ gesture: UIGestureRecognizer) {
         if let gestureView = gesture.view, gesture.state == .ended {
             let touchPoint: CGPoint = gesture.location(in: gestureView)
