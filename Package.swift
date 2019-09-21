caa04a196b71f199ab0cd34eeffca8c394d63823
shogo4405 <shogo4405@gmail.com>
2019-09-21 15:33:31 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,25 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "HaishinKit",
+    products: [
+        .library(name: "RTMP", targets: ["HTTP"]),
+        .library(name: "HTTP", targets: ["RTMP"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/shogo4405/Logboard.git", majorVersion: 1)
+        .Package(url: "https://github.com/shogo4405/Logboard.git", from: "2.2.3")
+    ],
+    targets: [
+        .target(name: "Codec", dependencies: [])
+        .target(name: "Extension", dependencied: [])
+        .target(name: "HTTP", dependencies: [])
+        .target(name: "ISO", dependencies: [])
+        .target(name: "Media", dependencies: [])
+        .target(name: "Util", dependencies: [])
+        .target(name: "Net", dependencies: ["Codec", "Extension", "ISO", "Media", "Util"])
+        .target(name: "HTTP", dependencies: ["Net"])
+        .target(name: "RTMP", dependencies: ["Net", "FLV"])
     ]
 )
