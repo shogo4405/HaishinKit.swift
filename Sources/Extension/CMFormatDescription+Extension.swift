69464f63848a52cd5984bcd72e3f6a5b8b7aaeb4
shogo4405 <shogo4405@gmail.com>
2019-12-01 19:31:22 +0900

    add obsoleted check

--- Sources/Extension/CMFormatDescription+Extension.swift
+++ Sources/Extension/CMFormatDescription+Extension.swift
@@ -2,10 +2,6 @@ import CoreMedia
 import Foundation
 
 extension CMFormatDescription {
-    var extensions: [String: AnyObject]? {
-        return CMFormatDescriptionGetExtensions(self) as? [String: AnyObject]
-    }
-
     func `extension`(by key: String) -> [String: AnyObject]? {
         return CMFormatDescriptionGetExtension(self, extensionKey: key as CFString) as? [String: AnyObject]
     }
--- Sources/Extension/CMVideoFormatDescription+Extension.swift
+++ Sources/Extension/CMVideoFormatDescription+Extension.swift
@@ -2,6 +2,9 @@ import CoreImage
 import CoreMedia
 
 extension CMVideoFormatDescription {
+    @available(iOS, obsoleted: 13.0)
+    @available(tvOS, obsoleted: 13.0)
+    @available(macOS, obsoleted: 10.15)
     var dimensions: CMVideoDimensions {
         return CMVideoFormatDescriptionGetDimensions(self)
     }
