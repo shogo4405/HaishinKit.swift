a9c97bd5f704b13a483cda6ba5a0a6fe3055836f
shogo4405 <shogo4405@gmail.com>
2020-11-08 18:58:22 +0900

    refs #769

--- Package.swift
+++ Package.swift
@@ -10,10 +10,10 @@ let package = Package(
         .macOS(.v10_11)
     ],
     products: [
-        .library(name: "HaishinKit", targets: ["HaishinKit"])
+        .library(name: "HaishinKit", type: .dynamic, targets: ["HaishinKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/shogo4405/Logboard.git", from: "2.2.0")
+        .package(url: "https://github.com/shogo4405/Logboard.git", from: "2.2.1")
     ],
     targets: [
         .target(name: "SwiftPMSupport"),
