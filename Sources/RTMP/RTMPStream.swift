fc40b21b10aa1034d0abcf51a5b57a77a0fee4c7
shogo4405 <shogo4405@gmail.com>
2021-07-24 21:21:43 +0900

    mv RTMPStreamDelegate -> RTMPStream

--- Sources/RTMP/RTMPStream.swift
+++ Sources/RTMP/RTMPStream.swift
@@ -1,5 +1,25 @@
 import AVFoundation
 
+public protocol RTMPStreamDelegate: AnyObject {
+    func rtmpStream(_ stream: RTMPStream, didPublishInsufficientBW connection: RTMPConnection)
+    func rtmpStream(_ stream: RTMPStream, didPublishSufficientBW connection: RTMPConnection)
+    func rtmpStream(_ stream: RTMPStream, didOutput audio: AVAudioBuffer, presentationTimeStamp: CMTime)
+    func rtmpStream(_ stream: RTMPStream, didOutput video: CMSampleBuffer)
+    func rtmpStream(_ stream: RTMPStream, didStatics connection: RTMPConnection)
+    func rtmpStreamDidClear(_ stream: RTMPStream)
+}
+
+public extension RTMPStreamDelegate {
+    func rtmpStream(_ stream: RTMPStream, didStatics connection: RTMPConnection) {
+    }
+
+    func rtmpStream(_ stream: RTMPStream, didOutput audio: AVAudioBuffer, presentationTimeStamp: CMTime) {
+    }
+
+    func rtmpStream(_ stream: RTMPStream, didOutput video: CMSampleBuffer) {
+    }
+}
+
 /**
  flash.net.NetStream for Swift
  */
