6d0e7f0d2789e6148556a367646780ac634e0260
EvgenPol <evgenpol97@gmail.com>
2022-01-27 17:40:38 +0300

    Update RTMPStream.swift

--- Sources/RTMP/RTMPStream.swift
+++ Sources/RTMP/RTMPStream.swift
@@ -628,7 +628,7 @@ extension RTMPStream: RTMPMuxerDelegate {
             message: RTMPVideoMessage(streamId: id, timestamp: UInt32(videoTimestamp), payload: buffer)
         ), locked: &mixer.videoIO.encoder.locked)
         if !videoWasSent {
-            logger.debug("first video frame was sent")
+            logger.debug("first video frame was sent111111")
         }
         videoWasSent = true
         info.byteCount.mutate { $0 += Int64(length) }
