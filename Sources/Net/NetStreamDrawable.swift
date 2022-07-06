2a75e45a13a6c72da99fcd852e2a3d02c990987c
shogo4405 <shogo4405@gmail.com>
2022-07-07 03:05:25 +0900

    modify declaration of NetStream

--- Sources/Net/NetStreamDrawable.swift
+++ Sources/Net/NetStreamDrawable.swift
@@ -6,18 +6,17 @@ public protocol NetStreamDrawable: AnyObject {
     #if !os(tvOS)
     /// Specifies the orientation of AVCaptureVideoOrientation.
     var orientation: AVCaptureVideoOrientation { get set }
-    
+
     /// Specifies the position of AVCaptureDevice.
     var position: AVCaptureDevice.Position { get set }
     #endif
-    
+
     /// The videoFormatDescription which is the current CMSampleBuffer.
     var videoFormatDescription: CMVideoFormatDescription? { get }
 
     /// Attaches a drawable to a new NetStream object.
     func attachStream(_ stream: NetStream?)
-    
+
     /// Enqueue a CMSampleBuffer? to draw.
     func enqueue(_ sampleBuffer: CMSampleBuffer?)
 }
-
