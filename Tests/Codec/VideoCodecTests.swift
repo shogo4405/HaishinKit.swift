4a8852bdc8d27c2d22893a670e04d3777af28a9b
shogo4405 <shogo4405@gmail.com>
2023-09-02 20:17:58 +0900

    Bump Xcode to 15.0.

--- Tests/Codec/VideoCodecTests.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import XCTest
-import AVFoundation
-
-@testable import HaishinKit
-
-final class VideoCodecTests: XCTestCase {
-}
-
--- Tests/Net/NetStreamTests.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import XCTest
-
-@testable import HaishinKit
-
-final class NetStreamTests: XCTestCase {
-}
