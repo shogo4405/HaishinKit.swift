71c3da3ff813f70fbe0494be3994f1342794588f
shogo4405 <shogo4405@gmail.com>
2023-12-07 23:35:24 +0900

    Remove unnecessary code.

--- Packages/RealityKitContent/README.md
+++
@@ -1,3 +0,0 @@
-# RealityKitContent
-
-A description of this package.
--- Packages/RealityKitContent/Sources/RealityKitContent/RealityKitContent.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-/// Bundle for the RealityKitContent project
-public let realityKitContentBundle = Bundle.module
