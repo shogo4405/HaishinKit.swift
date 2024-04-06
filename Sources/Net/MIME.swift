da15684f79d27bf2b71e1e07370be5d806308f2a
shogo4405 <shogo4405@gmail.com>
2024-04-06 18:31:30 +0900

    Remove unnecessary code.

--- Sources/Net/MIME.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum MIME: String {
-    case textPlain           = "text/plain"
-    case textHtml            = "text/html"
-    case applicationXMpegURL = "application/x-mpegURL"
-    case vndAppleMpegURL     = "application/vnd.apple.mpegURL"
-    case videoMP2T           = "video/MP2T"
-}
