65cbf34465d69c1e733af5d9a5b81a5c2adb0ad1
shogo4405 <shogo4405@gmail.com>
2024-04-09 21:31:41 +0900

    Improve handling of RTMP timestamps.

---
+++ Sources/RTMP/RTMPTimestamp.swift
@@ -0,0 +1,82 @@
+import AVFoundation
+import CoreMedia
+import Foundation
+
+protocol RTMPTimeConvertible {
+    var seconds: TimeInterval { get }
+}
+
+private let kRTMPTimestamp_defaultTimeInterval: TimeInterval = 0
+private let kRTMPTimestamp_compositiionTimeOffset = CMTime(value: 3, timescale: 30)
+
+struct RTMPTimestamp<T: RTMPTimeConvertible> {
+    private var startedAt = kRTMPTimestamp_defaultTimeInterval
+    private var updatedAt = kRTMPTimestamp_defaultTimeInterval
+    private var timedeltaFraction: TimeInterval = kRTMPTimestamp_defaultTimeInterval
+
+    mutating func update(_ value: T) -> UInt32 {
+        if startedAt == 0 {
+            startedAt = value.seconds
+            updatedAt = value.seconds
+            return 0
+        } else {
+            var timedelta = (value.seconds - updatedAt) * 1000
+            timedeltaFraction += timedelta.truncatingRemainder(dividingBy: 1)
+            if 1 <= timedeltaFraction {
+                timedeltaFraction -= 1
+                timedelta += 1
+            }
+            updatedAt = value.seconds
+            return UInt32(timedelta)
+        }
+    }
+
+    mutating func update(_ message: RTMPMessage, chunkType: RTMPChunkType) {
+        switch chunkType {
+        case .zero:
+            if startedAt == 0 {
+                startedAt = TimeInterval(message.timestamp) / 1000
+                updatedAt = TimeInterval(message.timestamp) / 1000
+            } else {
+                updatedAt = TimeInterval(message.timestamp) / 1000
+            }
+        default:
+            updatedAt += TimeInterval(message.timestamp) / 1000
+        }
+    }
+
+    mutating func clear() {
+        startedAt = kRTMPTimestamp_defaultTimeInterval
+        updatedAt = kRTMPTimestamp_defaultTimeInterval
+        timedeltaFraction = kRTMPTimestamp_defaultTimeInterval
+    }
+
+    func getCompositionTime(_ sampleBuffer: CMSampleBuffer) -> Int32 {
+        guard sampleBuffer.decodeTimeStamp.isValid, sampleBuffer.decodeTimeStamp != sampleBuffer.presentationTimeStamp else {
+            return 0
+        }
+        let compositionTime = (sampleBuffer.presentationTimeStamp + kRTMPTimestamp_compositiionTimeOffset).seconds - updatedAt
+        return Int32(compositionTime * 1000)
+    }
+}
+
+extension AVAudioTime: RTMPTimeConvertible {
+    var seconds: TimeInterval {
+        AVAudioTime.seconds(forHostTime: hostTime)
+    }
+}
+
+extension RTMPTimestamp where T == AVAudioTime {
+    var value: AVAudioTime {
+        return AVAudioTime(hostTime: AVAudioTime.hostTime(forSeconds: updatedAt))
+    }
+}
+
+extension CMTime: RTMPTimeConvertible {
+}
+
+extension RTMPTimestamp where T == CMTime {
+    var value: CMTime {
+        return CMTime(seconds: updatedAt, preferredTimescale: 1000)
+    }
+}
---
+++ Tests/RTMP/RTMPTimestampTests.swift
@@ -0,0 +1,43 @@
+import Foundation
+import XCTest
+import AVFoundation
+@testable import HaishinKit
+
+final class RTMPTimestampTests: XCTestCase {
+    func testCMTime() {
+        let times: [CMTime] = [
+            CMTime(value: 286340171565869, timescale: 1000000000),
+            CMTime(value: 286340204889958, timescale: 1000000000),
+            CMTime(value: 286340238223357, timescale: 1000000000),
+            CMTime(value: 286340271560111, timescale: 1000000000),
+            CMTime(value: 286340304906325, timescale: 1000000000),
+            CMTime(value: 286340338232723, timescale: 1000000000),
+            CMTime(value: 286340338232723, timescale: 1000000000),
+        ]
+        var timestamp = RTMPTimestamp<CMTime>()
+        XCTAssertEqual(0, timestamp.update(times[0]))
+        XCTAssertEqual(33, timestamp.update(times[1]))
+        XCTAssertEqual(33, timestamp.update(times[2]))
+        XCTAssertEqual(33, timestamp.update(times[3]))
+        XCTAssertEqual(34, timestamp.update(times[4]))
+        XCTAssertEqual(33, timestamp.update(times[5]))
+    }
+
+    func testAVAudioTime() {
+        let times: [AVAudioTime] = [
+            .init(hostTime: 6901294874500, sampleTime: 13802589749, atRate: 48000),
+            .init(hostTime: 6901295386500, sampleTime: 13802590773, atRate: 48000),
+            .init(hostTime: 6901295898500, sampleTime: 13802591797, atRate: 48000),
+            .init(hostTime: 6901296410500, sampleTime: 13802592821, atRate: 48000),
+            .init(hostTime: 6901296922500, sampleTime: 13802593845, atRate: 48000),
+            .init(hostTime: 6901297434500, sampleTime: 13802594869, atRate: 48000),
+        ]
+        var timestamp = RTMPTimestamp<AVAudioTime>()
+        XCTAssertEqual(0, timestamp.update(times[0]))
+        XCTAssertEqual(21, timestamp.update(times[1]))
+        XCTAssertEqual(21, timestamp.update(times[2]))
+        XCTAssertEqual(22, timestamp.update(times[3]))
+        XCTAssertEqual(21, timestamp.update(times[4]))
+        XCTAssertEqual(21, timestamp.update(times[5]))
+    }
+}
