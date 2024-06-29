f68c924ee7a897d41c5f2c74b15eee2093dc901e
shogo4405 <shogo4405@gmail.com>
2024-06-29 22:45:34 +0900

    // Temp     static nonisolated(unsafe) var `default`

--- Examples/iOS/Preference.swift
+++ Examples/iOS/Preference.swift
@@ -1,5 +1,6 @@
-struct Preference {
-    static var `default` = Preference()
+struct Preference: Sendable {
+    // Temp
+    static nonisolated(unsafe) var `default` = Preference()
 
     var uri: String? = "rtmp://192.168.1.6/live"
     var streamName: String? = "live"
