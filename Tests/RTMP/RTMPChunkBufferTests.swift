5ce406999d1f5b45e883737a3ee5020a143418a5
shogo4405 <shogo4405@gmail.com>
2024-10-18 23:04:26 +0900

    Write RTMPChunkBuffer.

--- Tests/RTMP/RTMPChunkBufferTests.swift
+++ Tests/RTMP/RTMPChunkBufferTests.swift
@@ -8,7 +8,7 @@ import Testing
     static let readDataBufferUnderflow = Data([2, 0, 0, 0, 0, 0, 4, 5, 0, 0, 0, 0, 0, 76, 75, 64, 2, 0, 0, 0, 0, 0, 5, 6, 0, 0, 0, 0, 0, 76, 75, 64, 2, 2, 0, 0, 0, 0, 0, 4, 1, 0, 0, 0, 0, 0, 0, 32, 0, 3, 0, 0, 0, 0, 0, 190, 20, 0, 0, 0, 0, 2, 0, 7, 95, 114, 101, 115, 117, 108, 116, 0, 63, 240, 0, 0, 0, 0, 0, 0, 3, 0, 6, 102, 109, 115, 86, 101, 114, 2, 0, 13, 70, 77, 83, 47, 51, 44, 48, 44, 49, 44, 49, 50, 51, 0, 12, 99, 97, 112, 97, 98, 105, 108, 105, 116, 105, 101, 115, 0, 64, 63, 0, 0, 0, 0, 0, 0, 0, 0, 9, 3, 0, 5, 108, 101, 118, 101, 108, 2, 0, 6, 115, 116, 97, 116, 117, 115, 0, 4, 99, 111, 100, 101, 2, 0, 29, 78, 101, 116, 67, 111, 110, 110, 101, 99, 116, 105, 111, 110, 46, 67, 111, 110, 110, 101, 99, 116, 46, 83, 117, 99, 99, 101, 115, 115, 0, 11, 100, 101, 115, 99, 114, 105, 112, 116, 105, 111, 110, 2, 0, 21, 67, 111, 110, 110, 101, 99, 116, 105, 111, 110, 32, 115, 117, 99, 99, 101, 101, 100, 101, 100, 46, 0, 14, 111, 98, 106, 101, 99, 116, 69, 110, 99, 111, 100, 105, 110, 103, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     @Test func read() {
-        let buffer = RTMPChunkBuffer(.init(count: 1024))
+        let buffer = RTMPChunkBuffer()
         buffer.put(Self.readData)
 
         do {
@@ -57,8 +57,8 @@ import Testing
         }
     }
 
-    @Test func read_BufferUnderflow() {
-        let buffer = RTMPChunkBuffer(.init(count: 1024))
+    @Test func readBufferUnderflow() {
+        let buffer = RTMPChunkBuffer()
         buffer.chunkSize = 8192
         buffer.put(Self.readDataBufferUnderflow)
 
@@ -89,10 +89,8 @@ import Testing
         }
     }
 
-    /*
-    func testWrite() {
-        let buffer = RTMPChunkBuffer(.init(count: 1024))
-        _ = buffer.putBasicHeader(.zero, chunkStreamId: RTMPChunk.StreamID.command.rawValue)
+    @Test func write() {
+        let buffer = RTMPChunkBuffer()
         let connection = RTMPCommandMessage(
             streamId: 0,
             transactionId: 0,
@@ -101,7 +99,23 @@ import Testing
             commandObject: nil,
             arguments: []
         )
-        _ = buffer.putMessage(.zero, chunkStreamId: RTMPChunk.StreamID.command.rawValue, message: connection)
+        let iterator = buffer.putMessage(.zero, chunkStreamId: 1, message: connection)
+        #expect(iterator.next() == Data([1, 0, 0, 0, 0, 0, 18, 20, 0, 0, 0, 0, 2, 0, 5, 104, 101, 108, 108, 111, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5]))
     }
-     */
+
+    @Test func writeChunkSize() {
+        let buffer = RTMPChunkBuffer()
+        let connection = RTMPCommandMessage(
+            streamId: 0,
+            transactionId: 0,
+            objectEncoding: .amf0,
+            commandName: [String](repeating: "a", count: 128 + 56).joined(),
+            commandObject: nil,
+            arguments: []
+        )
+        let iterator = buffer.putMessage(.zero, chunkStreamId: 1, message: connection)
+        #expect(iterator.next() == Data([1, 0, 0, 0, 0, 0, 197, 20, 0, 0, 0, 0, 2, 0, 184, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97]))
+        #expect(iterator.next() == Data([193, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 97, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5]))
+        #expect(iterator.next() == nil)
+    }
 }
