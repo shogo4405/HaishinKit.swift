17266d7488c01a2c3a1df3de357403d161fdfc89
talk-fun <50986316+talk-fun@users.noreply.github.com>
2024-12-09 14:55:31 +0800

    1

--- TFSDK/TFFilter.swift
+++ TFSDK/TFFilter.swift
@@ -23,7 +23,7 @@ final class TFMonochromeEffect: TFFilter {
             return image
         }
         filter.setValue(image, forKey: "inputImage")
-        filter.setValue(CIColor(red: 0.75, green: 0.75, blue: 0.75), forKey: "inputColor")
+        filter.setValue(CIColor(red: 1.0, green: 0.75, blue: 0.8), forKey: "inputColor")
         filter.setValue(1.0, forKey: "inputIntensity")
         
         guard let outputImage = filter.outputImage else {
@@ -32,6 +32,34 @@ final class TFMonochromeEffect: TFFilter {
         return outputImage
     }
 }
+//对比度
+//final class TFMonochromeEffect: TFFilter {
+//    let filter: CIFilter? = CIFilter(name: "CIColorControls")
+//
+//    override func execute(_ image: CIImage) -> CIImage {
+//        guard let filter = filter else {
+//            return image
+//        }
+//        
+//        // 设置输入图像
+//        filter.setValue(image, forKey: kCIInputImageKey)
+//        
+//        // 调整亮度（0.0 到 1.0，1.0 表示原始亮度）
+//        filter.setValue(0.5, forKey: kCIInputBrightnessKey) // 增加亮度
+//        
+//        // 调整对比度（0.0 到 4.0，1.0 表示原始对比度）
+//        filter.setValue(1.5, forKey: kCIInputContrastKey) // 增加对比度
+//        
+//        // 调整饱和度（0.0 到 2.0，1.0 表示原始饱和度）
+//        filter.setValue(1.2, forKey: kCIInputSaturationKey) // 增加饱和度
+//        
+//        guard let outputImage = filter.outputImage else {
+//            return image
+//        }
+//        
+//        return outputImage
+//    }
+//}
 
 //加水印
 final class TFPronamaEffect: TFFilter {
