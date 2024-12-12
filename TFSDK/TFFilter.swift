31f0aac6c63ec1a7ed07fd3c3e0614b5e3e03f06
talk-fun <50986316+talk-fun@users.noreply.github.com>
2024-12-12 11:35:08 +0800

    优化

--- TFSDK/TFFilter.swift
+++ TFSDK/TFFilter.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
-
+import CoreImage
+import GPUImage
+import GPUImageBeautifyFilter
 enum TFFilterType {
     case watermark //水印
     case filters  //滤镜
@@ -20,7 +22,7 @@ class TFFilter: VideoEffect {
     var watermarkFrame:CGRect = .zero
     let watermarkFilter: CIFilter? = CIFilter(name: "CISourceOverCompositing")
     
-    let mageBeautifyFilter = TFUImageBeautifyFilter()
+//    let mageBeautifyFilter = TFUImageBeautifyFilter()
 
     func calculateNewWatermarkFrame(originalFrame: CGRect, imageExtent: CGSize, screenBounds: CGRect) -> CGRect {
         // 计算缩放因子
@@ -68,33 +70,55 @@ class TFFilter: VideoEffect {
             UIGraphicsEndImageContext()
         }
         
-        return mageBeautifyFilter.apply(image)!
+        return self.applyFilter(to: image)!
     }
-    
-//    func execute(_ image: CIImage) -> CIImage {
-//      //水印
-//        if(type == .watermark)
-//        {
-//
-//            guard let watermark = watermark else { return image }
-//            guard let filter: CIFilter = watermarkFilter else {return image}
-//
-//            //image.extent.size 是摄像头分辨率的大小
-//            UIGraphicsBeginImageContext(image.extent.size)
-//            watermark.draw(in: watermarkFrame )
-//
-//            let pronama = CIImage(image: UIGraphicsGetImageFromCurrentImageContext()!, options: nil)
-//            UIGraphicsEndImageContext()
-//            filter.setValue(pronama!, forKey: "inputImage")
-//            filter.setValue(image, forKey: "inputBackgroundImage")
-//
-//            guard let outputImage = filter.outputImage else {
-//                return image
-//            }
-//            return outputImage
-//        }
-//
-//        return mageBeautifyFilter.apply(image)!
-//
-//    }
+    func applyFilter(to ciImage: CIImage) -> CIImage? {
+        // 将 CIImage 转换为 UIImage
+        guard let oldImage = convertCIImageToUIImage(ciImage) else { return nil }
+        
+        // 使用美颜效果的滤镜 GPUImageBeautifyFilter
+        let beautifyFilter = GPUImageBeautifyFilter()
+        
+        // 设置要渲染的区域
+        beautifyFilter.forceProcessing(at: oldImage.size)
+        
+        // 使用下一帧进行图像捕获
+        beautifyFilter.useNextFrameForImageCapture()
+        
+        // 设置图片数据源
+        let stillImageSource = GPUImagePicture(image: oldImage)
+        
+        // 添加上美颜效果的滤镜
+        stillImageSource?.addTarget(beautifyFilter)
+        
+        // 开始渲染
+        stillImageSource?.processImage()
+        
+        // 获取渲染后的图片
+        guard let filteredImage = beautifyFilter.imageFromCurrentFramebuffer() else { return nil }
+        
+        // 将 UIImage 转换回 CIImage
+        guard let convertedCIImage = convertUIImageToCIImage(filteredImage) else { return nil }
+        
+        return convertedCIImage
+    }
+
+    private func convertUIImageToCIImage(_ image: UIImage) -> CIImage? {
+        // 如果 UIImage 是基于 CGImage 创建的，可以直接转换
+        if let cgImage = image.cgImage {
+            return CIImage(cgImage: cgImage)
+        }
+        return nil
+    }
+
+    private func convertCIImageToUIImage(_ ciImage: CIImage) -> UIImage? {
+        // 创建 CIContext
+        let context = CIContext(options: nil)
+        
+        // 渲染 CIImage 到 CGImage
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return nil }
+        
+        // 将 CGImage 转换为 UIImage
+        return UIImage(cgImage: cgImage)
+    }
 }
