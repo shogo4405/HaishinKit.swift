e3828b16a7e639950028da1f7bac1a102fa4c179
shogo4405 <shogo4405@gmail.com>
2025-01-17 22:17:13 +0900

    Update Examples/Examples.xcodeproj

---
+++ Examples/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.10
+
+// Leave blank. This is only here so that Xcode doesn't display it.
+
+import PackageDescription
+
+let package = Package(
+    name: "Examples",
+    products: [],
+    targets: []
+)
+
